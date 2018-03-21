ba9ee16b084afb40575011ab95211a3fdc1f3563
chiggg <chig_1215@yahoo.co.jp>
2018-03-21 23:35:20 +0900

    Child Pages ShortcodeのHTMLをstorkのカード型にあわせる

--- html/wp-content/themes/jstork_custom/functions.php
+++ html/wp-content/themes/jstork_custom/functions.php
@@ -9,3 +9,28 @@ function theme_enqueue_styles() {
         array('style')
     );
 }
+
+//Child Pages ShortcodeのHTMLをstorkのカード型にあわせる
+add_filter("child-pages-shortcode-template", "saude_child_pages_shortcode_template");
+function saude_child_pages_shortcode_template($template) {
+    $template = <<<EOF
+<article class="post-list cf animated fadeIn post-%post_id% post type-post status-publish format-standard article cf" role="article">
+<a href="%post_url%" rel="bookmark" title="%post_title%" class="no-icon">
+
+
+<figure class="eyecatch noimg">
+%post_thumb%
+</figure>
+
+<section class="entry-content cf">
+<h1 class="h2 entry-title">%post_title%</h1>
+
+<div class="description"><p>%post_excerpt%</p>
+</div>
+
+</section>
+</a>
+</article>
+EOF;
+    return $template;
+}
