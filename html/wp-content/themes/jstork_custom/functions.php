482416d126bb76a3a219b94b7804ecfd0d7ecf0a
chiggg <chig_1215@yahoo.co.jp>
2018-05-15 09:02:50 +0900

    MNN用Basic認証の設定

--- html/wp-content/themes/jstork_custom/functions.php
+++ html/wp-content/themes/jstork_custom/functions.php
@@ -69,3 +69,15 @@ function saude_mnn_rewrite() {
     $wp_rewrite->add_rewrite_tag('%mnn%', '([0-9]+)', 'post_type=mnn&p=');
 }
 
+//MNNのbasic認証設定用関数（header.phpで呼ぶ）
+function saude_mnn_basic_auth($auth_list,$realm="Restricted Area",$failed_text="認証に失敗しました"){ 
+	if (isset($_SERVER['PHP_AUTH_USER']) and isset($auth_list[$_SERVER['PHP_AUTH_USER']])){
+	if ($auth_list[$_SERVER['PHP_AUTH_USER']] == $_SERVER['PHP_AUTH_PW']){
+	return $_SERVER['PHP_AUTH_USER'];
+	}
+	}
+	header('WWW-Authenticate: Basic realm="'.$realm.'"');
+	header('HTTP/1.0 401 Unauthorized');
+	header('Content-type: text/html; charset='.mb_internal_encoding());
+	die($failed_text);
+}
