18460d7aefd714bba6baf278c892672108a17f4b
chiggg <chig_1215@yahoo.co.jp>
2018-05-16 09:35:27 +0900

    MNNのサポート機能を追加、パンくず表示を調整

--- html/wp-content/themes/jstork_custom/functions.php
+++ html/wp-content/themes/jstork_custom/functions.php
@@ -41,11 +41,12 @@ function saude_create_post_type() {
     register_post_type( 'mnn', [ // 投稿タイプ名の定義
         'labels' => [
             'name'          => 'MNN', // 管理画面上で表示する投稿タイプ名
-            'singular_name' => 'mnn',    // カスタム投稿の識別名
+            'all_items' => 'MNN一覧',    // カスタム投稿の識別名
         ],
         'public'        => true,  // 投稿タイプをpublicにするか
         'has_archive'   => true, // アーカイブ機能ON/OFF
         'menu_position' => 5,     // 管理画面上での配置場所
+        'supports' => array('title','editor','thumbnail','custom-fields','excerpt','author','revisions'),  /* 機能を有効化 */
         'rewrite'            => array(  // パーマリンク設定
 			'slug' => 'mnn',
 			'with_front' => false,
@@ -80,4 +81,88 @@ function saude_mnn_basic_auth($auth_list,$realm="Restricted Area",$failed_text="
 	header('HTTP/1.0 401 Unauthorized');
 	header('Content-type: text/html; charset='.mb_internal_encoding());
 	die($failed_text);
+}
+
+/*********************
+パンくずナビ
+*********************/
+if (!function_exists('breadcrumb')) {
+    function breadcrumb($divOption = array("id" => "breadcrumb", "class" => "breadcrumb inner wrap cf")){
+        global $post;
+        $str ='';
+        if(!get_option('side_options_pannavi')){
+            if(!is_home()&&!is_front_page()&&!is_admin() ){
+                $tagAttribute = '';
+                foreach($divOption as $attrName => $attrValue){
+                    $tagAttribute .= sprintf(' %s="%s"', $attrName, $attrValue);
+                }
+                $str.= '<div'. $tagAttribute .'>';
+                $str.= '<ul>';
+                $str.= '<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. home_url() .'/" itemprop="url"><i class="fa fa-home"></i><span itemprop="title"> HOME</span></a></li>';
+         
+                if(is_category()) {
+                    $cat = get_queried_object();
+                    if($cat -> parent != 0){
+                        $ancestors = array_reverse(get_ancestors( $cat -> cat_ID, 'category' ));
+                        foreach($ancestors as $ancestor){
+                            $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_category_link($ancestor) .'" itemprop="url"><span itemprop="title">'. get_cat_name($ancestor) .'</span></a></li>';
+                        }
+                    }
+                    $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">'. $cat -> name . '</span></li>';
+                } elseif(is_single()){
+                    $categories = get_the_category($post->ID);
+                    $cat = $categories[0];
+                    if($cat -> parent != 0){
+                        $ancestors = array_reverse(get_ancestors( $cat -> cat_ID, 'category' ));
+                        foreach($ancestors as $ancestor){
+                            if(!empty(get_category_link($ancestor))) {
+                                $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_category_link($ancestor).'" itemprop="url"><span itemprop="title">'. get_cat_name($ancestor). '</span></a></li>';
+                            }
+                        }
+                    }
+                    if(!empty(get_category_link($cat -> term_id))) {
+                        $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_category_link($cat -> term_id). '" itemprop="url"><span itemprop="title">'. $cat-> cat_name . '</span></a></li>';
+                    }
+                    $str.= '<li>'. $post -> post_title .'</li>';
+                } elseif(is_page()){
+                    if($post -> post_parent != 0 ){
+                        $ancestors = array_reverse(get_post_ancestors( $post->ID ));
+                        foreach($ancestors as $ancestor){
+                            $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_permalink($ancestor).'" itemprop="url"><span itemprop="title">'. get_the_title($ancestor) .'</span></a></li>';
+                        }
+                    }
+                    $str.= '<li>'. $post -> post_title .'</li>';
+                } elseif(is_date()){
+                    if( is_year() ){
+                        $str.= '<li>' . get_the_time('Y') . '年</li>';
+                    } else if( is_month() ){
+                        $str.= '<li><a href="' . get_year_link(get_the_time('Y')) .'">' . get_the_time('Y') . '年</a></li>';
+                        $str.= '<li>' . get_the_time('n') . '月</li>';
+                    } else if( is_day() ){
+                        $str.= '<li><a href="' . get_year_link(get_the_time('Y')) .'">' . get_the_time('Y') . '年</a></li>';
+                        $str.= '<li><a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('n') . '月</a></li>';
+                        $str.= '<li>' . get_the_time('j') . '日</li>';
+                    }
+                    if(is_year() && is_month() && is_day() ){
+                        $str.= '<li>' . wp_title('', false) . '</li>';
+                    }
+                } elseif(is_search()) {
+                    $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">「'. get_search_query() .'」で検索した結果</span></li>';
+                } elseif(is_author()){
+                    $str .='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">投稿者 : '. get_the_author_meta('display_name', get_query_var('author')).'</span></li>';
+                } elseif(is_tag()){
+                    $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">タグ : '. single_tag_title( '' , false ). '</span></li>';
+                } elseif(is_attachment()){
+                    $str.= '<li><span itemprop="title">'. $post -> post_title .'</span></li>';
+                } elseif(is_404()){
+                    $str.='<li>ページがみつかりません。</li>';
+                } else{
+                    $str.='<li></li>';
+                }
+                $str.='</ul>';
+                $str.='</div>';
+            }
+        }
+        echo $str;
+    }
 }
